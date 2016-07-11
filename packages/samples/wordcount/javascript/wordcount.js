b8d8f1254f9580bf95da7f68eb34448acfa3afaa
Vincent Hou <shou@us.ibm.com>
2016-07-11 10:35:55 -0400

    Add test cases to the action wordcount under samples

--- packages/samples/wordcount/javascript/wordcount.js
+++ packages/samples/wordcount/javascript/wordcount.js
@@ -1,41 +1,10 @@
 /**
- * word count utility
- *
- * Return word count as a binary number. This demonstrates the use of a blocking
- * invoke.
+ *  word count utility
  */
 function main(params) {
-    var str = params.payload;
-    var binary = params.binary || false;
-    console.log("The payload is '" + str + "', binary is '" + binary + "'");
-
-    if (binary == "false") {
-        var words = str.split(" ");
-        var count = words.length;
-        console.log("The message '"+str+"' has", count, 'words');
-        return { count: count };
-    } else {
-        whisk.invoke({
-            name : 'wordCount',
-            parameters : {
-                payload : str
-            },
-            blocking : true,
-            next : function(error, activation) {
-                console.log('activation:', activation);
-		if (!error) {
-		    var wordsInDecimal = activation.result.count;
-		    var wordsInBinary = wordsInDecimal.toString(2) + ' (base 2)';
-		    whisk.done({
-		        binaryCount : wordsInBinary
-		    });
-		} else {
-		    console.log('error:', error);
-		    whisk.error(error);
-                }
-	    }
-	});
-	return whisk.async();
-    }
+    var str = params.payload.toString();
+    var words = str.split(" ");
+    var count = words.length;
+    console.log("The message '"+str+"' has", count, 'words');
+    return { count: count };
 }
-
---
+++ tests/src/packages/samples/WordCountTest.scala
@@ -0,0 +1,54 @@
+package packages.samples
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import scala.concurrent.duration.DurationInt
+import scala.language.postfixOps
+import org.junit.Assert.assertTrue
+
+import common.JsHelpers
+import common.TestHelpers
+import common.Wsk
+import common.WskProps
+import common.WskTestHelpers
+import spray.json.DefaultJsonProtocol.BooleanJsonFormat
+import spray.json.DefaultJsonProtocol.StringJsonFormat
+import spray.json.pimpAny
+
+@RunWith(classOf[JUnitRunner])
+class WordCountTest extends TestHelpers
+    with WskTestHelpers
+    with JsHelpers {
+
+    implicit val wskprops = WskProps()
+    val wsk = new Wsk(usePythonCLI=true)
+    val wordcountAction = "/whisk.system/samples/wordCount"
+
+    behavior of "samples wordCount"
+
+    it should "Return the number of words when sending the words as payload" in withAssetCleaner(wskprops) {
+        (wp, assetHelper) =>
+            val expectedNumber = 3
+            val run = wsk.action.invoke(wordcountAction,
+                Map("payload" -> "Five fuzzy felines".toJson))
+            withActivation(wsk.activation, run) {
+                activation =>
+                    activation.getFieldPath("response", "success") should be(Some(true.toJson))
+                    activation.getFieldPath("response", "result", "count").toString should be(
+                        Some(expectedNumber).toString)
+            }
+    }
+
+    it should "Return an error has occurred: TypeError: Cannot read property 'toString' of undefined " +
+        "failure when sending no payload" in withAssetCleaner(wskprops) {
+        (wp, assetHelper) =>
+            val expectedError = "An error has occurred: TypeError: Cannot read property 'toString' of undefined".toJson
+            val run = wsk.action.invoke(wordcountAction, Map())
+            withActivation(wsk.activation, run) {
+                activation =>
+                    activation.getFieldPath("response", "success") should be(Some(false.toJson))
+                    activation.getFieldPath("response", "result", "error") should be(
+                        Some(expectedError))
+            }
+    }
+}
