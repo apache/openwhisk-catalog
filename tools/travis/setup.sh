c25721c3d758056ea7d648f73124c7a4e3b123a5
Jason Peterson <jasonpet@us.ibm.com>
2016-08-10 13:00:15 -0400

    Update docker installation to use new travis images

--- tools/travis/setup.sh
+++ tools/travis/setup.sh
@@ -3,19 +3,19 @@
 SCRIPTDIR=$(cd $(dirname "$0") && pwd)
 HOMEDIR="$SCRIPTDIR/../../../"
 
-# Docker stuff
+sudo gpasswd -a travis docker
+sudo -E bash -c 'echo '\''DOCKER_OPTS="-H tcp://0.0.0.0:4243 -H unix:///var/run/docker.sock --api-enable-cors --storage-driver=aufs"'\'' > /etc/default/docker'
+
+# Docker
 sudo apt-get -y update -qq
-sudo apt-get -o Dpkg::Options::="--force-confnew" --force-yes -y install docker-engine=1.9.1-0~trusty
+sudo apt-get -o Dpkg::Options::="--force-confold" --force-yes -y install docker-engine=1.9.1-0~trusty
 docker --version
 
-# Setup docker
-sudo -E bash -c 'echo '\''DOCKER_OPTS="-H tcp://0.0.0.0:4243 -H unix:///var/run/docker.sock --api-enable-cors --storage-driver=aufs"'\'' >> /etc/default/docker'
-sudo gpasswd -a travis docker
-sudo service docker restart
-
-# Python stuff
+# Python
 sudo apt-get -y install python-pip
 pip install --user jsonschema
+
+# Ansible
 pip install --user ansible==2.0.2.0
 
 # OpenWhisk stuff
