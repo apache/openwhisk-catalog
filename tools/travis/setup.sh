cfb68924586abf9f6b37eeb92d60db6a97196e8b
Jason Peterson <jasonpet@us.ibm.com>
2016-10-19 11:57:11 -0400

    updating ansible and docker

--- tools/travis/setup.sh
+++ tools/travis/setup.sh
@@ -8,7 +8,7 @@ sudo -E bash -c 'echo '\''DOCKER_OPTS="-H tcp://0.0.0.0:4243 -H unix:///var/run/
 
 # Docker
 sudo apt-get -y update -qq
-sudo apt-get -o Dpkg::Options::="--force-confold" --force-yes -y install docker-engine=1.9.1-0~trusty
+sudo apt-get -o Dpkg::Options::="--force-confold" --force-yes -y install docker-engine=1.12.0-0~trusty
 docker --version
 
 # Python
@@ -16,7 +16,7 @@ sudo apt-get -y install python-pip
 pip install --user jsonschema
 
 # Ansible
-pip install --user ansible==2.0.2.0
+pip install --user ansible==2.1.2.0
 
 # OpenWhisk stuff
 
