4318041b26efaf147e651d959a38536e607da3df
Jason Peterson <jasonpet@us.ibm.com>
2016-10-19 13:06:23 -0400

    travis update

--- tools/travis/setup.sh
+++ tools/travis/setup.sh
@@ -9,7 +9,11 @@ sudo -E bash -c 'echo '\''DOCKER_OPTS="-H tcp://0.0.0.0:4243 -H unix:///var/run/
 # Docker
 sudo apt-get -y update -qq
 sudo apt-get -o Dpkg::Options::="--force-confold" --force-yes -y install docker-engine=1.12.0-0~trusty
-docker --version
+sudo service docker restart
+echo "Docker Version:"
+docker version
+echo "Docker Info:"
+docker info
 
 # Python
 sudo apt-get -y install python-pip
