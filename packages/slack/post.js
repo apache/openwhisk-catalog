4a28515d0d7349beb0e8ef04ab833ced4bc0adb9
Rodric Rabbah <rabbah@us.ibm.com>
2017-01-21 17:35:17 -0500

    Update post message action to allow attachments correctly.

--- packages/slack/post.js
+++ packages/slack/post.js
@@ -1,7 +1,7 @@
-var request = require('request');
+var request = require('request-promise');
 
 /**
- *   Action to post to slack
+ * Action to post to slack
  *  @param {string} url - Slack webhook url
  *  @param {string} channel - Slack channel to post the message to
  *  @param {string} username - name to post the message as
@@ -9,81 +9,75 @@ var request = require('request');
  *  @param {string} icon_emoji - (optional) emoji to use as the icon for the message
  *  @param {boolean} as_user - (optional) when the token belongs to a bot, whether to post as the bot itself
  *  @param {object} attachments - (optional) message attachments (see Slack documentation for format)
- *  @return {object} whisk async
+ *  @return {object} Promise
  */
 function main(params) {
-    var promise = new Promise(function (resolve, reject) {
-      checkParams(params, reject);
+    let errorMsg = checkParams(params);
+    if (errorMsg) return { error: errorMsg };
 
-      var body = {
+    var body = {
         channel: params.channel,
-        username: params.username || 'Simple Message Bot',
-        text: params.text
-      };
+        username: params.username || 'Simple Message Bot'
+    };
 
-      if (params.icon_emoji) {
+    if (params.icon_emoji) {
         // guard against sending icon_emoji: undefined
         body.icon_emoji = params.icon_emoji;
-      }
+    }
 
-      if (params.token) {
+    if (params.as_user === true) {
+        body.as_user = true;
+    }
+
+    if (params.text) {
+        body.text = params.text;
+    }
+
+    if (params.attachments) {
+        body.attachments = JSON.stringify(params.attachments);
+    }
+
+    if (params.token) {
         //
         // this allows us to support /api/chat.postMessage
         // e.g. users can pass params.url = https://slack.com/api/chat.postMessage
         //                 and params.token = <their auth token>
         //
         body.token = params.token;
-      } else {
+    } else {
         //
         // the webhook api expects a nested payload
         //
         // notice that we need to stringify; this is due to limitations
         // of the formData npm: it does not handle nested objects
         //
-        console.log(body);
-        console.log("to: " + params.url);
-
         body = {
-          payload: JSON.stringify(body)
+            payload: JSON.stringify(body)
         };
-      }
+    }
 
-      if (params.as_user === true) {
-          body.as_user = true;
-      }
-
-      if (params.attachments) {
-          body.attachments = params.attachments;
-      }
-
-      request.post({
+    let options = {
+        method: 'POST',
         url: params.url,
-        formData: body
-      }, function (err, res, body) {
-        if (err) {
-          console.log('error: ', err, body);
-          reject(err);
-        } else {
-          console.log('success: ', params.text, 'successfully sent');
-          resolve();
-        }
-      });
-    });
+        formData: body,
+        json: true
+    };
 
-    return promise;
+    return request(options);
 }
 
 /**
-Checks if all required params are set
-*/
-function checkParams(params, reject) {
-  if (params.text === undefined) {
-    reject('No text provided');
-  }
-  if (params.url === undefined) {
-	reject('No Webhook URL provided');
-  }
-  if (params.channel === undefined) {
-	reject('No channel provided');
-  }
+ *  Checks if all required params are set.
+ */
+function checkParams(params) {
+    if (params.text === undefined && params.attachments === undefined) {
+        return ('No text provided');
+    }
+    else if (params.url === undefined) {
+        return 'No Webhook URL provided';
+    }
+    else if (params.channel === undefined) {
+        return 'No channel provided';
+    }
+    else return undefined;
 }
