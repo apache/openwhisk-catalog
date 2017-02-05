a507a931b08325a82129cb574ca49e46767433fe
Rodric Rabbah <rabbah@us.ibm.com>
2017-02-05 10:12:09 -0500

    Combinators actions.
    1. eca: event-condition-action
    1. forwarder: forwards incoming parameters around an action
    3. retry: retry action invoke up to specified limit
    4. try-cath: wrap action with a try and run catch handler on error
    
    Installer script.
    Tests.

---
+++ packages/installCombinators.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#
+# use the command line interface to install standard actions deployed
+# automatically.
+#
+: ${WHISK_SYSTEM_AUTH:?"WHISK_SYSTEM_AUTH must be set and non-empty"}
+AUTH_KEY=$WHISK_SYSTEM_AUTH
+
+SCRIPTDIR="$(cd $(dirname "$0")/ && pwd)"
+PACKAGE_HOME=$SCRIPTDIR
+source "$PACKAGE_HOME/util.sh"
+
+echo Installing action combinator package.
+PKGNAME=combinators
+
+createPackage "$PKGNAME" -a description "Actions combinators for richer composition"
+
+waitForAll
+
+install "$PACKAGE_HOME/$PKGNAME/eca.js" \
+     "$PKGNAME/eca" \
+     -a description 'Event-Condition-Action: run condition action and iff result is successful, run action.' \
+     -a parameters '[
+         { "name": "$conditionName", "required": true, "type": "string", "description": "Name of action to run to compute condition. Must return error to indicate false predicate." },
+         { "name": "$actionName",    "required": true, "type": "string", "description": "Name of action to run if result of condition action is not error." } ]' \
+     -a sampleInput '{ "$conditionName": "utils/echo", "$actionName": "utils/echo", "error": true }' \
+     -a sampleOutput '{ "$eca": "Condition was false" }'
+
+install "$PACKAGE_HOME/$PKGNAME/forwarder.js" \
+     "$PKGNAME/forwarder" \
+     -a description 'Forward parameters around another action.' \
+     -a parameters '[
+          { "name": "$actionName", "required": true, "type": "string", "description": "Name of action to run to compute condition. Must return error to indicate false predicate." },
+          { "name": "$actionArgs", "required": true, "type": "array",  "description": "Array of parameters names from input arguments to pass to action." },
+          { "name": "$forward",    "required": true, "type": "array",  "description": "Array of parameters names from input arguments to merge with result of action." } ]' \
+     -a sampleInput '{ "$actionName": "utils/echo", "$actionArgs": [ "x" ], "$forward": [ "y" ], "x": true, "y": true }' \
+     -a sampleOutput '{ "x": true, "y": true }'
+
+install "$PACKAGE_HOME/$PKGNAME/retry.js" \
+     "$PKGNAME/retry" \
+     -a description 'Retry invoking an action until success or attempt count is exhausted, which ever comes first.' \
+     -a parameters '[
+         { "name": "$actionName", "required": true, "type": "string", "description": "Name of action to run." },
+         { "name": "$attempts",   "required": true, "type": "integer", "description": "Name of attempts, must be >= 1." } ]' \
+     -a sampleInput '{ "$actionName": "utils/echo", "error": true }' \
+     -a sampleOutput '{ "error": "Invocation failed. No retries left." }'
+
+install "$PACKAGE_HOME/$PKGNAME/trycatch.js" \
+     "$PKGNAME/trycatch" \
+     -a description 'Wraps an action with a try-catch. If the action fails, invokes a second action to handle the error.' \
+     -a parameters '[
+          { "name": "$tryName",   "required": true, "type": "string", "description": "Name of action to wrap with a try." },
+          { "name": "$catchName", "required": true, "type": "string", "description": "Name of action to run if there is a try error." } ]' \
+     -a sampleInput '{ "$tryName": "utils/echo", "$catchName": "utils/echo", "error": true }' \
+     -a sampleOutput '{ "error": true }'
+
+waitForAll
+
+echo combinator package ERRORS = $ERRORS
+exit $ERRORS
