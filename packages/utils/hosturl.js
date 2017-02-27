41e0ab8fe7d13600920215d5f517a7daf0dc20e8
Jason Peterson <jasonpet@us.ibm.com>
2017-02-27 11:15:43 -0500

    updating jshint defaults

--- packages/utils/hosturl.js
+++ packages/utils/hosturl.js
@@ -16,16 +16,18 @@ function main(args) {
     if (args.web === true && args.trigger) {
         return { error: 'triggers are not allowed for web actions.' };
     } else if (args.web === true && args.ext) {
-        if (args.ext != '.json' && args.ext != '.text' && args.ext != '.html' && args.ext != '.http') {
+        if (args.ext !== '.json' && args.ext !== '.text' && args.ext !== '.html' && args.ext !== '.http') {
             return { error: 'extension for web action must be one of [".json", ".text", ".html", ".http"].' };
         }
-    } else if (args.trigger && args.path.indexOf('/') != -1) {
+    } else if (args.trigger && args.path.indexOf('/') !== -1) {
         return { error: 'triggers are not allowed in packages.' };
     }
 
     if (args.path) {
-        let path = args.web === true && args.path.indexOf('/') == -1 ? `default/${args.path}` : args.path;
+        let path = args.web === true && args.path.indexOf('/') === -1 ? `default/${args.path}` : args.path;
         let ext = args.web === true ? (args.ext || '.json') : '';
         return { url: `${base}/${path}${ext}` };
-    } else return { url: base };
+    } else {
+        return { url: base };
+    }
 }
