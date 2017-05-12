9ae646ecfe06555500bc137b067c8caac4cb88ec
Carlos Santana <csantana@us.ibm.com>
2017-05-12 16:23:27 -0400

    git clone to dir openwhisk

--- tools/travis/setup.sh
+++ tools/travis/setup.sh
@@ -9,7 +9,7 @@ sudo npm install -g jshint
 
 # OpenWhisk stuff
 cd $HOMEDIR
-git clone --depth 1 https://github.com/apache/incubator-openwhisk.git
+git clone --depth 1 https://github.com/apache/incubator-openwhisk.git openwhisk
 
 cd openwhisk
 ./tools/travis/setup.sh
