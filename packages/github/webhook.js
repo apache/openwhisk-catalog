1fe973ef34e5148b3092df3a40bc1e50932739f6
Matt Rutkowski <mrutkows@us.ibm.com>
2017-06-27 14:08:22 -0500

    update remaining files with ASF headers.

--- packages/github/webhook.js
+++ packages/github/webhook.js
@@ -1,7 +1,11 @@
+// Licensed to the Apache Software Foundation (ASF) under one or more contributor
+// license agreements; and to You under the Apache License, Version 2.0.
+
 var request = require('request');
 
 /**
- *   Feed to create a webhook on Github
+ *  Feed to create a webhook on Github
+ *
  *  @param {object} params - information about the trigger
  *  @param {string} repository - repository to create webhook
  *  @param {string} username - github username
