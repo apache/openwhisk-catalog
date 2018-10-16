8dc5dca1962b3b08d3d17a7989830e484c2f2ac1
Prabhash <prabhash.15@cse.mrt.ac.lk>
2018-10-16 11:21:49 +0530

    Update JiraTest.scala

--- tests/src/test/scala/packages/jira/JiraTest.scala
+++ tests/src/test/scala/packages/jira/JiraTest.scala
@@ -1,5 +1,5 @@
 
-package packages.slack
+package packages.jira
 
 import org.junit.runner.RunWith
 import org.scalatest.BeforeAndAfterAll
@@ -13,23 +13,28 @@ class JiraTests extends TestHelpers with WskTestHelpers with BeforeAndAfterAll {
   implicit val wskprops = WskProps()
   val wsk = new Wsk()
 
-  val credentials = TestUtils.getCredentials("slack_webhook")
+  val credentials = TestUtils.getCredentials("jira_webhook")
   val username = credentials.get("username").getAsString()
   val siteName = credentials.get("siteName").getAsString()
   val accessToken = credentials.get("accessToken").getAsString()
   val webhookName = credentials.get("webhookName").getAsString()
   val force_http = credentials.get("force_http").getAsString()
-  val events = credentials.get("events").getAsString().split(',') //split this
-  val myJiraFeed = credentials.get("channel").getAsString()
+  val triggerName = "/_/" + credentials.get("triggerName").getAsString()
+  val events = credentials.get("events").getAsString()
+
+  val jirafeeds = "/whisk.system/jira/jirafeed"
 
   "Jira Package" should "print the webhook created on JIRA" in {
-    val run = wsk.trigger.create(myJiraFeed, Map(
+    val run = wsk.action.invoke(jirafeeds, Map(
       "username" -> username.toJson,
       "siteName" -> siteName.toJson,
       "accessToken" -> accessToken.toJson,
       "webhookName" -> webhookName.toJson,
       "force_http" -> force_http.toJson,
-      "events" -> events.split(,)toJson))   //can be a syntax error
+      "triggerName" -> triggerName.toJson,
+      "lifecycleEvent" -> "CREATE".toJson,
+      "events" -> events.toJson))
+
     withActivation(wsk.activation, run) {
       activation =>
         activation.response.success shouldBe true
@@ -39,13 +44,15 @@ class JiraTests extends TestHelpers with WskTestHelpers with BeforeAndAfterAll {
   }
 
   "Jira Package" should "print the webhook deleted on JIRA" in {
-    val run = wsk.trigger.delete(myJiraTrigger, Map(
+    val run = wsk.action.invoke(jirafeeds, Map(
       "username" -> username.toJson,
       "siteName" -> siteName.toJson,
       "accessToken" -> accessToken.toJson,
-      //        "webhookName" -> webhookName.toJson,
-      "force_http" -> force_http.toJson))
-    //        "events" -> events.toJson))                       //this should be splitted
+      "webhookName" -> webhookName.toJson,
+      "force_http" -> force_http.toJson,
+      "triggerName" -> triggerName.toJson,
+      "lifecycleEvent" -> "DELETE".toJson,
+      "events" -> events.toJson))
     withActivation(wsk.activation, run) {
       activation =>
         activation.response.success shouldBe true
