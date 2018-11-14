c43ae46560225b4af17746b88240fa916c4159e8
Priti Desai <pdesai@us.ibm.com>
2018-11-14 13:02:19 -0800

    updating wskdeploy instructions

--- tools/travis/build.sh
+++ tools/travis/build.sh
@@ -38,20 +38,20 @@ cd $WHISKDIR/ansible
 
 ANSIBLE_CMD="ansible-playbook -i environments/local"
 
-###$ANSIBLE_CMD setup.yml
-###$ANSIBLE_CMD prereq.yml
-###$ANSIBLE_CMD couchdb.yml
-###$ANSIBLE_CMD initdb.yml
-###$ANSIBLE_CMD properties.yml
+$ANSIBLE_CMD setup.yml
+$ANSIBLE_CMD prereq.yml
+$ANSIBLE_CMD couchdb.yml
+$ANSIBLE_CMD initdb.yml
+$ANSIBLE_CMD properties.yml
 
 cd $WHISKDIR
 
-###TERM=dumb ./gradlew install distDocker
+TERM=dumb ./gradlew install distDocker
 
 cd $WHISKDIR/ansible
 
-###$ANSIBLE_CMD wipe.yml
-###$ANSIBLE_CMD openwhisk.yml
+$ANSIBLE_CMD wipe.yml
+$ANSIBLE_CMD openwhisk.yml
 
 # Set Environment
 export OPENWHISK_HOME=$WHISKDIR
@@ -65,18 +65,18 @@ tar xf "$HOMEDIR/wskdeploy.tgz"
 
 # Install Catalog
 
-###cat $WHISKDIR/whisk.properties
+cat $WHISKDIR/whisk.properties
 cd $ROOTDIR/packages
 
 ./installCatalogUsingWskdeploy.sh "$HOMEDIR/wskdeploy"
 #./installCatalog.sh $WHISKDIR/ansible/files/auth.whisk.system
 
 # Set credentials
-###VCAP_SERVICES_FILE="$(readlink -f $ROOTDIR/tests/credentials.json)"
-###WHISKPROPS_FILE="$WHISKDIR/whisk.properties"
-###sed -i 's:^[ \t]*vcap.services.file[ \t]*=\([ \t]*.*\)$:vcap.services.file='$VCAP_SERVICES_FILE':'  $WHISKPROPS_FILE
+VCAP_SERVICES_FILE="$(readlink -f $ROOTDIR/tests/credentials.json)"
+WHISKPROPS_FILE="$WHISKDIR/whisk.properties"
+sed -i 's:^[ \t]*vcap.services.file[ \t]*=\([ \t]*.*\)$:vcap.services.file='$VCAP_SERVICES_FILE':'  $WHISKPROPS_FILE
 
 # Test
 
 cd $ROOTDIR
-###TERM=dumb ./gradlew :tests:testWithoutCredentials
+TERM=dumb ./gradlew :tests:testWithoutCredentials
